d53e10967b7a47882b3c1660512664f6ae6c9bf2
Khalid <alrmalykhalid7@gmail.com>
2026-03-30 07:02:02 +0300

    full dashboard system

--- server.js
+++ server.js
@@ -165,5 +165,32 @@ app.get('/api/dashboard', auth, admin, async (req,res)=>{
     res.json({users,orders});
 });
 
+app.get('/api/products', async (req,res)=>{
+    try{
+
+        const page = parseInt(req.query.page) || 1;
+        const limit = parseInt(req.query.limit) || 6;
+
+        const skip = (page - 1) * limit;
+
+        const products = await Product.find()
+            .skip(skip)
+            .limit(limit);
+
+        const total = await Product.countDocuments();
+
+        res.json({
+            products,
+            total,
+            page,
+            pages: Math.ceil(total / limit)
+        });
+
+    }catch(err){
+        console.error(err);
+        res.json({success:false});
+    }
+});
+
 // ================= SERVER =================
 app.listen(3000,()=>console.log("🔥 Tebex System Ready"));
